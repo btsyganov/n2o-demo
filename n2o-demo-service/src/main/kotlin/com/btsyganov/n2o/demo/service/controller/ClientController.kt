976a62af9c8878a661c694b1beb3bfba7d0f8f83
btsyganov <btsyganov@i-novus.ru>
2020-10-01 20:01:01 +0300

    modify main client controller

--- n2o-demo-service/src/main/kotlin/com/btsyganov/n2o/demo/service/controller/ClientController.kt
+++ n2o-demo-service/src/main/kotlin/com/btsyganov/n2o/demo/service/controller/ClientController.kt
@@ -1,7 +1,11 @@
 package com.btsyganov.n2o.demo.service.controller
 
+import com.btsyganov.n2o.demo.service.entity.Client
 import com.btsyganov.n2o.demo.service.repo.ClientRepository
+import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Sort
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -9,5 +13,15 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class ClientController(private val clientRepository: ClientRepository) {
     @GetMapping("/clients")
-    fun findAll(@RequestParam page: Int, @RequestParam size: Int) = clientRepository.findAll(PageRequest.of(page, size))
+    fun findAll(@RequestParam page: Int, @RequestParam size: Int, @RequestParam(required = false) sort: String?): Page<Client> {
+        val request: Pageable
+        request = if (sort != null && sort.split(",").size == 2) {
+            val sortField = sort.split(",")[0]
+            val sortDirection = sort.split(",")[1]
+            PageRequest.of(page - 1, size, Sort.by(if ("asc" == sortDirection) Sort.Direction.ASC else Sort.Direction.DESC, sortField))
+        } else
+            PageRequest.of(page - 1, size)
+
+        return clientRepository.findAll(request)
+    }
 }
