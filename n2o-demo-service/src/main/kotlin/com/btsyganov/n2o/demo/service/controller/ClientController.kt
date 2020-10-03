1168572f5907a33c225c5f4e4ae27158a6c83a37
btsyganov <btsyganov@i-novus.ru>
2020-10-03 11:47:09 +0300

    add findById method

--- n2o-demo-service/src/main/kotlin/com/btsyganov/n2o/demo/service/controller/ClientController.kt
+++ n2o-demo-service/src/main/kotlin/com/btsyganov/n2o/demo/service/controller/ClientController.kt
@@ -7,6 +7,7 @@ import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Pageable
 import org.springframework.data.domain.Sort
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
@@ -23,5 +24,10 @@ class ClientController(private val clientRepository: ClientRepository) {
             PageRequest.of(page - 1, size)
 
         return clientRepository.findAll(request)
+    }
+
+    @GetMapping("/clients/{id}")
+    fun findById(@PathVariable id: Int): Client {
+        return clientRepository.findById(id).orElseThrow()
     }
 }
